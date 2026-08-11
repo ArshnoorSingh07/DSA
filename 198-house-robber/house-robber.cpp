b294cc13d61cac0144e16eb3e523e5ce47377b85
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-08-11 12:12:47 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (97.83%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,12 +4,18 @@ public:
 
     int solve(int n, vector<int>&nums)
     {
-        if(n == 0) return nums[0];
-        if(n < 0) return 0;
-        if(dp[n] != -1) return dp[n];
-        int left = nums[n] + solve(n-2, nums);
-        int right = 0 + solve(n-1, nums);
-        return dp[n] = max(left, right);
+        if(n==0) return nums[0];
+        dp[0] = nums[0];
+        for(int i = 1; i< nums.size(); i++)
+        {
+            int pick = nums[i];
+            if(i > 1){
+                pick+=dp[i-2];
+            }
+            int notpick = 0 + dp[i-1];
+            dp[i] = max(pick, notpick);
+        }
+        return dp[n];
     }
 
     int rob(vector<int>& nums) {
