2c8ea4113c8348cf540f778b33849cdcccee13f4
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-08-14 14:48:07 +0530

    Time: 34 ms (43.17%) | Memory: 36.3 MB (67.87%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -20,7 +20,26 @@ public:
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1,-1));
-        return solve(n-1, amount, coins,dp);
+        vector<vector<int>>dp(n, vector<int>(amount+1,0));
+
+        for(int tar = 0; tar <= amount; tar++) {
+            if(tar % coins[0] == 0) {
+                dp[0][tar] = 1;
+            }
+        }
+
+        for(int ind = 1; ind < n; ind++)
+        {
+            for(int tar = 0; tar<= amount; tar++)
+            {
+                long long notTake = dp[ind-1][tar];
+                long long take = 0;
+                if(coins[ind] <= tar){
+                    take = dp[ind][tar - coins[ind]];
+                }
+                dp[ind][tar] = take + notTake;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
