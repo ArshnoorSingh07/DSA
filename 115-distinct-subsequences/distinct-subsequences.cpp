aca122e6b9496101774895b2159d28ea99deca9e
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-08-18 15:34:19 +0530

    Time: 39 ms (20.99%) | Memory: 27 MB (53.95%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -4,7 +4,7 @@ public:
     {
         if(j<0) return 1;
         if(i<0) return 0;
-        
+
         if(dp[i][j] != -1) return dp[i][j];
 
         if(s1[i] == s2[j]){
@@ -17,7 +17,7 @@ public:
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>>dp(n, vector<int>(m+1,-1));
+        vector<vector<int>>dp(n, vector<int>(m,-1));
         return solve(n-1,m-1,s,t,dp);
     }
 };
