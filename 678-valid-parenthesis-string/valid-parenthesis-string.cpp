d35abdcbb717711f1a7dd7080204a001f7ccb0b5
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-08-27 16:17:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (27.66%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -25,7 +25,31 @@ public:
 
     bool checkValidString(string s) {
         int n = s.size();
-        vector<vector<int>>dp(n, vector<int>(n+1,-1));
-        return solve(s, 0, 0,dp);
+        int min = 0;
+        int max = 0;
+        for(int i = 0; i<n;i++)
+        {
+            if(s[i] =='('){
+                min++;
+                max++;
+            }
+            else if(s[i] ==')'){
+                min--;
+                max--;
+            }
+            else{
+                min--;
+                max++;
+            }
+            if(min < 0){
+                min = 0;
+            }
+            if(max < 0){
+                return false;
+            }
+        }
+
+        return (min == 0);
+        
     }
 };
