81f758019bda9bc7880b8658e8bc76833e07167b
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-08-27 18:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 68.1 MB (21.13%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,33 +2,63 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int>left(n);
-
-        left[0] = 1;
-        for(int i = 1; i<n; i++){
-            if(ratings[i] > ratings[i-1]){
-                left[i] = left[i-1] + 1;
-            }else{
-                left[i] = 1;
-            }
-        }
-
-        int curr = 1;
-        int right = 1;
-        int sum = max(1, left[n-1]);
-
-        for(int i = n-2; i>=0; i--)
+        int i = 1;
+        int sum = 1;
+        while(i < n)
         {
-            if(ratings[i] > ratings[i+1]){
-                curr = right +1;
-                right = curr;
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                sum += 1;
+                continue;
             }
-            else{
-                curr= 1;
-                right =1;
+            int peak=1;
+            while(i < n && ratings[i] > ratings[i-1]){
+                peak+=1;
+                sum+=peak;
+                i++;
             }
-            sum += max(left[i], curr);
+            int down = 1;
+            while(i < n && ratings[i] < ratings[i-1]){
+                sum+=down;
+                i++;
+                down++;
+            }
+            if(down > peak){
+                sum+=down-peak;
+            }
         }
-        return sum; 
+
+        return sum;
+
+
+        // int n = ratings.size();
+        // vector<int>left(n);
+
+        // left[0] = 1;
+        // for(int i = 1; i<n; i++){
+        //     if(ratings[i] > ratings[i-1]){
+        //         left[i] = left[i-1] + 1;
+        //     }else{
+        //         left[i] = 1;
+        //     }
+        // }
+
+        // int curr = 1;
+        // int right = 1;
+        // int sum = max(1, left[n-1]);
+
+        // for(int i = n-2; i>=0; i--)
+        // {
+        //     if(ratings[i] > ratings[i+1]){
+        //         curr = right +1;
+        //         right = curr;
+        //     }
+        //     else{
+        //         curr= 1;
+        //         right =1;
+        //     }
+        //     sum += max(left[i], curr);
+        // }
+        // return sum; 
     }
 };
