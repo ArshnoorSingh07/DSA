778a26e7d9ce09a9a6e687839d4c8dae0287c0ca
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-08-27 17:31:06 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (96.40%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -22,7 +22,18 @@ public:
 
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n, -1);
-        return solve(0, nums,dp);
+        int jumps = 0;
+        int l = 0, r =0 ;
+        while(r < n-1)
+        {
+            int farthest = 0;
+            for(int ind = l; ind <= r; ind++){
+                farthest = max(farthest, ind + nums[ind]);
+            }
+            l = r+1;
+            r = farthest;
+            jumps++;
+        }
+        return jumps;
     }
 };
