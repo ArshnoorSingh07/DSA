083bef3587d47cf197324cf50b91e256e7197ca8
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-09-05 01:26:45 +0530

    Time: 25 ms (71.11%) | Memory: 36.5 MB (64.09%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-    int solve(int ind, int t, vector<int>&coins, vector<vector<int>>&dp){
-        
+
+    int solve(int ind, int T, vector<int>&coins,vector<vector<int>>&dp){
         if(ind == 0){
-            if(t % coins[0] == 0){
+            if(T % coins[0] == 0){
                 return 1;
             }
             else return 0;
         }
 
-        if(dp[ind][t] != -1) return dp[ind][t];
-        
-        int notTake = solve(ind-1, t, coins, dp);
-        int take = 0; 
-        if(coins[ind] <= t){
-            take = solve(ind, t - coins[ind], coins, dp); 
+        if(dp[ind][T] != -1){
+            return dp[ind][T];
         }
-        return dp[ind][t] = take + notTake;
+
+        int notTake = solve(ind-1, T, coins,dp);
+        int take = 0;
+        if(coins[ind] <= T){
+            take = solve(ind, T- coins[ind], coins,dp);
+        }
+        return dp[ind][T] = take + notTake;
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<int>prev(amount+1,0), curr(amount+1,0);
-
-        for(int tar = 0; tar <= amount; tar++) {
-            if(tar % coins[0] == 0) {
-                prev[tar] = 1;
-            }
-        }
-
-        for(int ind = 1; ind < n; ind++)
-        {
-            for(int tar = 0; tar<= amount; tar++)
-            {
-                long long notTake = prev[tar];
-                long long take = 0;
-                if(coins[ind] <= tar){
-                    take = curr[tar - coins[ind]];
-                }
-                curr[tar] = take + notTake;
-            }
-            prev = curr;
-        }
-        return prev[amount];
+        vector<vector<int>>dp(n, vector<int>(amount+1,-1));
+        return solve(n-1, amount, coins,dp);
     }
 };
