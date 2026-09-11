0ca8d1f057f226fb103ba75049e9dda22edb4e79
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-09-11 14:34:17 +0530

    Time: 1340 ms (19.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+SELECT s.student_id, s.student_name, sub.subject_name, count(e.subject_name)       as attended_exams 
+    FROM Students s
+    CROSS JOIN Subjects sub  -- Create first table of both essentials
+
+    -- Now perform the task
+    LEFT JOIN Examinations e
+    ON sub.subject_name = e.subject_name
+    AND s.student_id = e.student_id
+
+    GROUP BY
+        s.student_id,
+        s.student_name,
+        sub.subject_name
+    
+    ORDER BY 
+        s.student_id, sub.subject_name;
