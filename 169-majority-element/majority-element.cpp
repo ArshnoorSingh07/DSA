2f519f7d08dd695d54f012426f1d972f3feefefb
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-09-16 17:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (16.42%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,18 +2,48 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        // Using hashing
-        unordered_map<int,int>mpp;
+        // Optimal approach
+        int el = 0;
+        int cnt = 0;
         for(int i = 0; i<n; i++){
-            mpp[nums[i]]++;
-        }
-
-        for(auto &it: mpp){
-            if(it.second > n/2){
-                return it.first;
+            if(cnt == 0){
+                cnt = 1;
+                el = nums[i];
+            }
+            else if(nums[i] == el){
+                cnt++;
+            }
+            else{
+                cnt--;
             }
         }
+
+        cnt = 0;
+        for(int i = 0; i<n;i++){
+            if(nums[i] == el){
+                cnt++;
+            }
+        }
+
+        if(cnt > n/2){
+            return el;
+        }
+
         return -1;
+
+
+        // Using hashing
+        // unordered_map<int,int>mpp;
+        // for(int i = 0; i<n; i++){
+        //     mpp[nums[i]]++;
+        // }
+
+        // for(auto &it: mpp){
+        //     if(it.second > n/2){
+        //         return it.first;
+        //     }
+        // }
+        // return -1;
 
         // BRUTE FORCE:
         // for(int i = 0; i<n; i++)
