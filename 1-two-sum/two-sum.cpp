b7ff09ce4021acde86981183736fc73c6b2f4fba
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-09-19 11:31:55 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (28.84%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0; i<nums.size(); i++){
             int needed = target - nums[i];
             if(mpp.find(needed) != mpp.end()){
-                return {mpp[needed], i};
+                return {mpp[needed],i};
             }
             mpp[nums[i]] = i;
         }
