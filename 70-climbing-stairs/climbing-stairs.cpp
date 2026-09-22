90947f0ccfd77980dbc3605d4f176743b760c1f5
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-09-22 14:19:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (87.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -8,12 +8,17 @@ public:
         if(dp[n] != -1){
             return dp[n];
         }
-
         return dp[n] = solve(n - 1) + solve(n - 2);
     }
 
     int climbStairs(int n) {
         memset(dp,-1, sizeof(dp));
-        return solve(n);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i<=n; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
