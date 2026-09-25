aecc3eff0b84047324822501b1d2bebf2237240d
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-09-25 14:41:27 +0530

    Time: 11 ms (27.81%) | Memory: 11 MB (18.67%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -11,7 +11,7 @@ public:
             mpp[s[r] - 'A']++;
             maxFreq = max(maxFreq, mpp[s[r]-'A']);
 
-            if(abs(maxFreq - (r-l+1)) > k){
+            while((r-l+1) - maxFreq > k){
                 mpp[s[l] - 'A']--;
                 l++;
             }
