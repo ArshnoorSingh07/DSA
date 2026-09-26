9ac24a42f584a5efffe443bd527b861338d1e30a
Arshnoor Singh <arshnoorsingh05@gmail.com>
2026-09-26 17:41:48 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.97%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int sum = 0;
-        int total = n * (n + 1) /2;
-        for(auto it: nums){
-            sum+=it;
-        }
-        return total - sum;
-
         // int n = nums.size();
-        // int ans = n;
-        // for(int i = 0; i<nums.size(); i++)
-        // {
-        //     ans ^= i ^ nums[i];
+        // int sum = 0;
+        // int total = n * (n + 1) /2;
+        // for(auto it: nums){
+        //     sum+=it;
         // }
-        // return ans;
+        // return total - sum;
+
+        int n = nums.size();
+        int ans = n;
+        for(int i = 0; i<nums.size(); i++)
+        {
+            ans ^= i ^ nums[i];
+        }
+        return ans;
     }
 };
